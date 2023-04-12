49d458a66c49b6918e40766428ed6980403647d4
mike@nimbusintelligence.com <mike@nimbusintelligence.com>
2023-04-12 06:02:43 +0000

    added snapshots

---
+++ snapshots/snapshot_orders.sql
@@ -0,0 +1,18 @@
+{% snapshot orders %}
+
+{%set new_schema = target.schema + '_snapshot'%}
+
+{{
+    config(
+      target_database='DEMO_PRD',
+      target_schema=new_schema,
+      unique_key='order_id',
+
+      strategy='timestamp',
+      updated_at='updated_at'
+    )
+}}
+
+select * from DEMO_RAW,{{target.schema}}.orders
+
+{% endsnapshot %}
