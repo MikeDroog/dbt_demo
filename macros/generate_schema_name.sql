91d6127738ad2940754c9028c11a06b4cd14af4a
mike@nimbusintelligence.com <mike@nimbusintelligence.com>
2023-04-14 08:58:42 +0000

    added schema_change

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,15 +1,23 @@
--- By default, custom schema name will be combined with target.schema name
--- Override this schema to based on requirements
-
 {% macro generate_schema_name(custom_schema_name, node) -%}
+
     {%- set default_schema = target.schema -%}
-    
+
     {%- if custom_schema_name is none -%}
+
         {{ default_schema }}
-        {{ log("Setting Default Schema: {0}".format(target.schema)) }}
+
     {%- else -%}
-        {{ custom_schema_name | trim }}
-        {{ log("Setting Custom Schema: {0}".format(custom_schema_name | trim)) }}
+
+        {%- if target.name.startswith('dev') -%}
+
+            {{ default_schema }}_{{ custom_schema_name | trim }}
+
+        {%- else -%}
+
+            {{ custom_schema_name | trim }}
+
+        {%- endif -%}
+
     {%- endif -%}
 
 {%- endmacro %}
